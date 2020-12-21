6d2423e70a59595527263b65458c947d1e13cb50
Cole <70500105+ImPurpl3@users.noreply.github.com>
2020-12-20 22:35:49 -0600

    Add files via upload
    added colour to the console

--- eCTP-rpc.py
+++ eCTP-rpc.py
@@ -1,11 +1,23 @@
 from pypresence import Presence
 import time
+import sty
+from sty import fg, RgbFg
 
 client_id = '786012261964709888'
 RPC = Presence(client_id)
 RPC.connect()
 
-print(RPC.update(state="Receiving Therapy",details="eCrew Therapy Program" ,large_image="cus_up", large_text="HIM", small_image="fail", small_text="Failure"))
+fg.set_style('statSet', RgbFg(66, 135, 245))
+stat = fg.statSet + 'Status Set!'
+
+fg.set_style('startUp', RgbFg(57, 230, 106))
+start = fg.startUp + 'Starting Up!'
+
+print(start)
+time.sleep(1)
+
+RPC.update(state="Receiving Therapy",details="eCrew Therapy Program" ,large_image="cus_up", large_text="HIM", small_image="fail", small_text="Failure")
+print(stat)
 
 while True:
     time.sleep(1)
